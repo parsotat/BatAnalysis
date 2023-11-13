0c9d020f337778b16a695e1dbf85260e79ff7d48
David Palmer <palmer@lanl.gov>
2023-11-13 13:47:27 -0700

    One file at a time

--- setup.py
+++ setup.py
@@ -5,46 +5,52 @@ steps to upload a distribution to PyPi is at: https://stackoverflow.com/question
 from setuptools import setup, find_packages
 import os
 
-#also need to include all subdirs in the data directory
+
+# also need to include all subdirs in the data directory
 def package_files(directory):
     paths = []
-    for (path, directories, filenames) in os.walk(directory):
+    for path, directories, filenames in os.walk(directory):
         for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
+            paths.append(os.path.join("..", path, filename))
     return paths
 
+
 try:
-    with open("README.md", 'r') as f:
+    with open("README.md", "r") as f:
         long_description = f.read()
 except FileNotFoundError:
-    long_description = ''
+    long_description = ""
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-extra_files = package_files('./batanalysis/data')
+extra_files = package_files("./batanalysis/data")
 
-#get the version number from the _version file
+# get the version number from the _version file
 with open("batanalysis/_version.py") as f:
     file_info = f.read().splitlines()
-version=file_info[-1].split("=")[-1].split('"')[1]
+version = file_info[-1].split("=")[-1].split('"')[1]
 
 setup(
-    name='BatAnalysis',
+    name="BatAnalysis",
     version=version,
-    packages=['batanalysis'],
-    url='https://github.com/parsotat/BatAnalysis',
-    license='BSD-3-Clause',
-    author='Tyler Parsotan and Sibasish Laha',
-    author_email='tyler.parsotan@nasa.gov',
-    description='Routines for analyzing data from BAT on the Neil Gehrels Swift Observatory',
+    packages=["batanalysis"],
+    url="https://github.com/parsotat/BatAnalysis",
+    license="BSD-3-Clause",
+    author="Tyler Parsotan and Sibasish Laha",
+    author_email="tyler.parsotan@nasa.gov",
+    description="Routines for analyzing data from BAT on the Neil Gehrels Swift Observatory",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Science/Research', 'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Astronomy', ],
-    python_requires='>=3.8',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    python_requires=">=3.8",
     install_requires=required,
-    package_data={'': extra_files},
+    package_data={"": extra_files},
     include_package_data=True,
 )
