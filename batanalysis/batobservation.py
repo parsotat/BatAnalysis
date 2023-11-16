c9d892f18acc2b9ece711edfb5e70deb02157de5
Tyler Parsotan <parsotat@umbc.edu>
2023-11-16 11:42:20 -0500

    Added relevant methods for the Spectrum class from the Lightcurve class.

--- batanalysis/batobservation.py
+++ batanalysis/batobservation.py
@@ -1163,10 +1163,11 @@ class Spectrum(BatObservation):
         if not self.pha_file.exists() or recalc:
             #see if the input dict is None so we can set these defaults, otherwise save the requested inputs for use later
             if pha_input_dict is None:
-                self.pha_input_dict = dict(infile=str(self.event_file), outfile=str(self.lightcurve_file), outtype="PHA",
-                              energybins="15-350", weighted="YES", timedel=0.064,
+                #energybins = "CALDB" gives us the 80 channel spectrum
+                self.pha_input_dict = dict(infile=str(self.event_file), outfile=str(self.pha_file), outtype="PHA",
+                              energybins="CALDB", weighted="YES", timedel=0.0,
                               detmask=str(self.detector_quality_mask),
-                              tstart="INDEF", tstop="INDEF", clobber="YES", timebinalg="uniform")
+                              tstart="INDEF", tstop="INDEF", clobber="YES")
 
             else:
                 self.pha_input_dict = pha_input_dict
@@ -1267,4 +1268,199 @@ class Spectrum(BatObservation):
         :return:
         """
 
-        return calc_response()
+        return calc_response(self.pha_file)
+
+    def _get_event_weights(self):
+        """
+        This method reads in the appropriate weights for event data once it has been applied to a event file, for a
+        given RA/DEC position. This should only need to be done once, when the user has applied mask weighting
+        in the BatEvent object and is creating a lightcurve.
+
+        :return: None
+        """
+
+        #read in all the info for the weights and save it such that we can use these weights in the future for
+        #redoing lightcurve calculation
+        with fits.open(self.event_file) as file:
+            self._event_weights=file[1].data["MASK_WEIGHT"]
+
+
+
+
+    def _set_event_weights(self):
+        """
+        This method sets the appropriate weights for event data, for a
+        given RA/DEC position. The weights are rewritten to the event file in the "MASK_WEIGHT" column.
+        This may be necessary if a user is analyzing multiple sources for which event data has been
+        obtained.
+
+        Note: event weightings need to be set if the RA/DEC of the light curve doesnt match what is in the event file
+
+        :return: None
+        """
+
+        if not self._same_event_lc_coords():
+            #read in the event file and replace the values in the MASK_WEIGHT with the appropriate values in self._event_weights
+            with fits.open(self.event_file, mode="update") as file:
+                file[1].data["MASK_WEIGHT"]=self._event_weights
+                file.flush()
+
+    def _same_event_lc_coords(self):
+        """
+        This method reads in the event data coordinates and compares it to what is obained from the lightcurve
+        file that has been loaded in.
+
+        :return: Boolean
+        """
+
+        with fits.open(self.event_file) as file:
+            event_ra = file[0].header["RA_OBJ"]
+            event_dec = file[0].header["DEC_OBJ"]
+            coord_match = (event_ra == self.lc_ra) and (event_dec == self.lc_dec)
+
+        return coord_match
+
+    def _parse_pha_file(self):
+        """
+        This method parses through a light curve file that has been created by batbinevent. The information included in
+        the lightcurve file is read into the RA/DEC attributes (and checked to make sure that this is the lightcurve that
+        the user wants to load in), the data attribute (which holds the light curve information itself including rates/counts,
+        errors, fracitonal exposure, total counts, etc), the ebins attribute which holds the energybins associated with
+        the lightcurve, the tbins attibute with the time bin edges and the time bin centers
+
+        NOTE: A special value of timepixr=-1 (the default used when constructing light curves) specifies that
+              timepixr=0.0 for uniformly binned light curves and
+              timepixr=0.5 for non-unformly binned light curves. We recommend using the unambiguous TIME_CENT in the
+              tbins attribute to prevent any confusion instead of the data["TIME"] values
+
+        :param calc_energy_integrated: Boolean to denote if the energy integrated lightcurve should be calculated or not.
+            By default, it is calculated unless the user has created a lightcurve of only 1 energy bin.
+        :return: None
+        """
+
+        #error checking for calc_energy_integrated
+        if type(calc_energy_integrated) is not bool:
+            raise ValueError("The calc_energy_integrated parameter should be a boolean value.")
+
+
+        with fits.open(self.lightcurve_file) as f:
+            header=f[1].header
+            data=f[1].data
+            energies=f["EBOUNDS"].data
+            energies_header=f["EBOUNDS"].header
+
+        if self.lc_ra is None and self.lc_dec is None:
+            self.lc_ra = header["RA_OBJ"]
+            self.lc_dec = header["DEC_OBJ"]
+        else:
+            #test if the passed in coordinates are what they should be for the light curve file
+            #TODO: see if we are ~? arcmin close to one another
+            assert (np.isclose(self.lc_ra, header["RA_OBJ"]) and np.isclose(self.lc_dec, header["DEC_OBJ"])), \
+                   f"The passed in RA/DEC values ({self.lc_ra},{self.lc_dec}) do not match the values used to produce the lightcurve which are ({header['RA_OBJ']},{header['DEC_OBJ']})"
+
+        #read in the data and save to data attribute which is a dictionary of the column names as keys and the numpy arrays as values
+        self.data={}
+        for i in data.columns:
+            self.data[i.name] = u.Quantity(data[i.name], i.unit)
+
+        #fill in the energy bin info
+        self.ebins={}
+        for i in energies.columns:
+            if "CHANNEL" in i.name:
+                self.ebins["INDEX"] = energies[i.name]
+            elif "E" in i.name:
+                self.ebins[i.name]=u.Quantity(energies[i.name], i.unit)
+
+        #fill in the time info separately
+        timepixr=header["TIMEPIXR"]
+        #see if there is a time delta column exists for variable time bin widths
+        if "TIMEDEL" not in self.data.keys():
+            dt=header["TIMEDEL"]*u.s
+        else:
+            dt=self.data["TIMEDEL"]
+
+
+        self.tbins = {}
+        #see https://heasarc.gsfc.nasa.gov/ftools/caldb/help/batbinevt.html
+        self.tbins["TIME_CENT"] = self.data["TIME"] + (0.5-timepixr)*dt
+        self.tbins["TIME_START"] = self.data["TIME"] - timepixr*dt
+        self.tbins["TIME_STOP"] = self.data["TIME"] + (1-timepixr)*dt
+
+        #if self.lc_input_dict ==None, then we will need to try to read in the hisotry of parameters passed into batbinevt
+        # to create the lightcurve file. thsi usually is needed when we first parse a file so we know what things are if we need to
+        # do some sort of rebinning.
+
+        #were looking for something like:
+        # START PARAMETER list for batbinevt_1.48 at 2023-11-01T20:38:05
+        #
+        # P1 infile = /Users/tparsota/Documents/01116441000_eventresult/events/sw0
+        # P1 1116441000bevshsp_uf.evt
+        # P2 outfile = 01116441000_eventresult/lc/lightcurve_0.lc
+        # P3 outtype = LC
+        # P4 timedel = 0.064
+        # P5 timebinalg = uniform
+        # P6 energybins = 15-350
+        # P7 gtifile = NONE
+        # P8 ecol = ENERGY
+        # P9 weighted = YES
+        # P10 outunits = INDEF
+        # P11 timepixr = -1.0
+        # P12 maskwt = NONE
+        # P13 tstart = INDEF
+        # P14 tstop = INDEF
+        # P15 snrthresh = 6.0
+        # P16 detmask = /Users/tparsota/Documents/01116441000_eventresult/auxil/sw
+        # P16 01116441000bdqcb.hk.gz
+        # P17 tcol = TIME
+        # P18 countscol = DPH_COUNTS
+        # P19 xcol = DETX
+        # P20 ycol = DETY
+        # P21 maskwtcol = MASK_WEIGHT
+        # P22 ebinquant = 0.1
+        # P23 delzeroes = no
+        # P24 minfracexp = 0.1
+        # P25 min_dph_frac_overlap = 0.999
+        # P26 min_dph_time_overlap = 0.0
+        # P27 max_dph_time_nonoverlap = 0.5
+        # P28 buffersize = 16384
+        # P29 clobber = yes
+        # P30 chatter = 2
+        # P31 history = yes
+        # P32 mode = ql
+        # END PARAMETER list for batbinevt_1.48
+
+        if self.lc_input_dict is None:
+            #get the default names of the parameters for batbinevt including its name 9which should never change)
+            test = hsp.HSPTask('batbinevt')
+            default_params_dict=test.default_params.copy()
+            taskname=test.taskname
+            start_processing=None
+
+            for i in header["HISTORY"]:
+                if taskname in i and start_processing is None:
+                    #then set a switch for us to start looking at things
+                    start_processing = True
+                elif taskname in i and start_processing is True:
+                    #we want to stop processing things
+                    start_processing = False
+
+                if start_processing and "START" not in i and len(i)>0:
+                    values=i.split(" ")
+                    #print(i, values, "=" in values)
+
+                    parameter_num=values[0]
+                    parameter=values[1]
+                    if "=" not in values:
+                        #this belongs with the previous parameter and is a line continuation
+                        default_params_dict[old_parameter] = default_params_dict[old_parameter] + values[-1]
+                    else:
+                        default_params_dict[parameter] = values[-1]
+
+                    old_parameter=parameter
+
+            self.lc_input_dict = default_params_dict.copy()
+
+        if calc_energy_integrated:
+            self._calc_energy_integrated()
+
+
