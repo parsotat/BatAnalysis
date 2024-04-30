cd7bf8f05e2047b50552670a3dcf23287817b5c3
Tyler Parsotan <parsotat@umbc.edu>
2024-04-30 11:48:27 -0400

    Put back imports into the __init__ file.

--- batanalysis/__init__.py
+++ batanalysis/__init__.py
@@ -8,3 +8,16 @@ __all__ = [
     "mosaic",
 ]
 
+from ._version import __version__
+
+from .batobservation import *
+from .batproducts import *
+from .bat_survey import *
+from .bat_tte import *
+from .bat_dph import *
+from .bat_dpi import *
+from .bat_skymap import *
+from .batlib import *
+from .plotting import *
+from .mosaic import *
+from . import parallel
