35c5aa0a69d84c9547e44c2da2fed5cb40602bee
Tyler Parsotan <parsotat@umbc.edu>
2024-05-15 15:07:45 -0400

    Created bat_skyview to hold the BatSkyView class which holds all the necessary information generated by batfftimage.

--- batanalysis/bat_skyimage.py
+++ batanalysis/bat_skyimage.py
@@ -20,12 +20,6 @@ from histpy import Histogram, HealpixAxis
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from reproject import reproject_to_healpix
 
-try:
-    import heasoftpy as hsp
-except ModuleNotFoundError as err:
-    # Error handling
-    print(err)
-
 
 class BatSkyImage(Histogram):
     """
@@ -525,82 +519,3 @@ class BatSkyImage(Histogram):
         max_e = energy_data["E_MAX"] * energy_unit.unit
 
         return cls(image_data=img_data, tmin=min_t, tmax=max_t, emin=min_e, emax=max_e, wcs=w)
-
-
-class BatSkyView(object):
-    """
-    This class holds the information related to a sky image, which is created from a detector plane image
-
-    This is constructed by doing a FFT of the sky image with the detector mask. There can be one or many
-    energy or time bins. We can also handle the projection of the image onto a healpix map.
-
-    TODO: create a python FFT deconvolution. This primarily relies on the batfftimage to create the data.
-    """
-
-    def __init__(
-            self,
-            skyimg_file=None,
-            dpi_file=None,
-            detector_quality_file=None,
-            attitude_file=None,
-            dpi_data=None,
-            input_dict=None,
-            recalc=False,
-            load_dir=None,
-            bkg_dpi_file=None,
-            create_bkgvar_map=True,
-            create_pcode_map=True
-    ):
-        """
-
-        :param skyimg_file:
-        :param dpi_file:
-        :param detector_quality_file:
-        :param attitude_file:
-        :param dpi_data:
-        :param input_dict:
-        :param recalc:
-        :param load_dir:
-        :param bkg_dpi_file:
-        """
-
-        if dpi_data is not None:
-            raise NotImplementedError(
-                "Dealing with the DPI data directly to calculate the sky image is not yet supported.")
-
-        if skyimg_file is not None:
-            self.skyimg_file = Path(skyimg_file).expanduser().resolve()
-
-        # do some error checking
-        if detector_quality_file is not None:
-            self.detector_quality_file = Path(detector_quality_file).expanduser().resolve()
-            if not self.detector_quality_file.exists():
-                raise ValueError(
-                    f"The specified detector quality mask file {self.detector_quality_file} does not seem "
-                    f"to exist. Please double check that it does.")
-        else:
-            self.detector_quality_file = None
-            # warnings.warn("No detector quality mask file has been specified. The resulting DPI object "
-            #              "will not be able to be modified either by rebinning in energy or time.", stacklevel=2)
-
-    def _call_batfftimage(self, input_dict):
-        """
-        Calls heasoftpy's batfftimage with an error wrapper, ensures that no runtime errors were encountered
-
-        :param input_dict: Dictionary of inputs that will be passed to heasoftpy's batfftimage
-        :return: heasoftpy Result object from batfftimage
-
-        :param input_dict:
-        :return:
-        """
-
-        input_dict["clobber"] = "YES"
-        input_dict["outtype"] = "DPI"
-
-        try:
-            return hsp.batfftimage(**input_dict)
-        except Exception as e:
-            print(e)
-            raise RuntimeError(
-                f"The call to Heasoft batfftimage failed with inputs {input_dict}."
-            )
