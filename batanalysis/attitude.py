b23ef6aa3e264871d26ac246e0fb312a7c30904e
Tyler Parsotan <parsotat@umbc.edu>
2024-07-24 16:34:23 -0400

    Started to create from_file class method for the Attitude class.

--- batanalysis/attitude.py
+++ batanalysis/attitude.py
@@ -4,3 +4,35 @@ the attitude data.
 
 Tyler Parsotan July 2024
 """
+from pathlib import Path
+
+import astropy.units as u
+from astropy.io import fits
+
+
+class Attitude(object):
+    """
+    This class encapsulates the Swift attitude data contained in a *.sat or *.mkf file that is obtained by Swift
+
+    TODO: add methods to add/concatenate event data, plot event data, etc
+    """
+
+    def __init__(self):
+        """
+        Itialize something
+        """
+        return None
+
+    @classmethod
+    def from_file(cls, attitude_file):
+        attitude_file = Path(attitude_file).expanduser().resolve()
+
+        if not attitude_file.exists():
+            raise ValueError(f"The attitude file passed in to be read {attitude_file} does not seem to exist.")
+
+        # iteratively read in the data with units
+        all_data = {}
+        with fits.open(attitude_file) as file:
+            data = file[1].data
+            for i in data.columns:
+                all_data[i.name] = u.Quantity(data[i.name], i.unit)
